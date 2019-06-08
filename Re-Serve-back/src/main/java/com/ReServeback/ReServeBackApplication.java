1f51baa41294b1c7f68f565697d97961bc6cabd8
Vincent <bluekiller57@gmail.com>
2019-06-08 18:08:15 -0400

    added front and back end

---
+++ Re-Serve-back/src/main/java/com/ReServeback/ReServeBackApplication.java
@@ -0,0 +1,13 @@
+package com.ReServeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReServeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReServeBackApplication.class, args);
+	}
+
+}
